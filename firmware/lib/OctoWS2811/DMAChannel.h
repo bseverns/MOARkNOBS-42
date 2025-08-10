301980f0728669505510484b85d55a304e4e829e
Ben Severns <severns3@gmail.com>
2025-08-10 12:04:48 -0500

    Wrap DMAChannel and document vendored OctoWS2811

---
+++ firmware/lib/OctoWS2811/DMAChannel.h
@@ -0,0 +1,5 @@
+#pragma once
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#include_next <DMAChannel.h>
+#pragma GCC diagnostic pop
---
+++ firmware/lib/OctoWS2811/OctoWS2811_imxrt.cpp
@@ -0,0 +1,4 @@
+#include "OctoWS2811.h"
+#include "DMAChannel.h"
+
+// Stub implementation for Teensy 4.x
