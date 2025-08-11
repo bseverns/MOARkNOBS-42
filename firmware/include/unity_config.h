a47a2bef86260d8ec0e6cccfa1510ccc9f32eb7b
Ben Severns <severns3@gmail.com>
2025-08-11 13:22:10 -0500

    feat: support host stdio for Unity logs

--- firmware/include/unity_config.h
+++ firmware/include/unity_config.h
@@ -1,11 +1,23 @@
 #pragma once
 
+#ifdef ARDUINO
 #include <Arduino.h>
+#else
+#include <cstdio>
+#endif
 
 // Unity wants to know where to spit its test logs.
-// Route every character over Serial and keep the rest quiet.
+// On Teensy we shout over Serial; on the host we spew to stdout.
+// This split personality keeps both toolchains grinning.
+#ifdef ARDUINO
 #define UNITY_OUTPUT_START()      Serial.begin(115200)
 #define UNITY_OUTPUT_CHAR(c)      Serial.write(c)
 #define UNITY_OUTPUT_FLUSH()      Serial.flush()
 #define UNITY_OUTPUT_COMPLETE()   Serial.end()
+#else
+#define UNITY_OUTPUT_START()
+#define UNITY_OUTPUT_CHAR(c)      putchar(c)
+#define UNITY_OUTPUT_FLUSH()      fflush(stdout)
+#define UNITY_OUTPUT_COMPLETE()
+#endif
 
