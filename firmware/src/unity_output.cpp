b1650281df50c6fc861e998baed04be690df6afe
Ben Severns <severns3@gmail.com>
2025-08-12 11:21:17 -0500

    guard Unity output for builds without USB_MIDI_SERIAL

--- firmware/src/unity_output.cpp
+++ firmware/src/unity_output.cpp
@@ -1,22 +1,37 @@
 #include <Arduino.h>
+#include <cstdio>
 #include "unity_config.h"
 
 extern "C" {
 
 void unityOutputStart(unsigned long baudrate) {
+#ifdef USB_MIDI_SERIAL
   Serial.begin(baudrate);
+#else
+  (void)baudrate; // No-op when the USB serial gadget isn't around
+#endif
 }
 
 void unityOutputChar(unsigned int c) {
+#ifdef USB_MIDI_SERIAL
   Serial.write(static_cast<uint8_t>(c)); // Unity slings ints; Serial chews bytes
+#else
+  putchar(static_cast<char>(c));
+#endif
 }
 
 void unityOutputFlush() {
+#ifdef USB_MIDI_SERIAL
   Serial.flush();
+#else
+  fflush(stdout);
+#endif
 }
 
 void unityOutputComplete() {
+#ifdef USB_MIDI_SERIAL
   Serial.end();
+#endif
 }
 
 }
