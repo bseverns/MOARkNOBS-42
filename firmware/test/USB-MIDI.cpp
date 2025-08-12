9a2f08cb25c1e7aee21d11127827337b3e6ae1e6
Ben Severns <severns3@gmail.com>
2025-08-12 15:55:13 -0500

    unity adjustments

--- firmware/test/USB-MIDI.cpp
+++ firmware/test/USB-MIDI.cpp
@@ -5,10 +5,8 @@
 #undef usbMIDI
 MidiInterfaceStub MIDI;
 USBMidiStub usbMIDI;
-#ifndef ARDUINO
 HardwareSerial Serial1;
 
 SerialStub Serial;
-#endif  // !ARDUINO
 #endif  // defined(UNIT_TEST) && defined(USB_MIDI_STUB)
 
