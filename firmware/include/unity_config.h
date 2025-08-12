9377eacf48a5c7333412ae3824130ea14385d79b
Ben Severns <severns3@gmail.com>
2025-08-12 10:30:20 -0500

    remove conflict

--- firmware/include/unity_config.h
+++ firmware/include/unity_config.h
@@ -14,13 +14,10 @@
 extern "C" {
 #endif
 
-<<<<<<< Updated upstream
 void unityOutputStart(unsigned long baudrate);
 void unityOutputChar(char c);
-=======
 void unityOutputStart();
 void unityOutputChar(unsigned int c);
->>>>>>> Stashed changes
 void unityOutputFlush();
 void unityOutputComplete();
 
@@ -34,13 +31,10 @@ void unityOutputComplete();
 #define UNITY_OUTPUT_COMPLETE()   unityOutputComplete()
 
 #ifndef ARDUINO
-<<<<<<< Updated upstream
 static inline void unityOutputStart(unsigned long baudrate) { (void)baudrate; }
 static inline void unityOutputChar(char c) { putchar(c); }
-=======
 static inline void unityOutputStart(void) {}
 static inline void unityOutputChar(unsigned int c) { putchar(c); }
->>>>>>> Stashed changes
 static inline void unityOutputFlush(void) { fflush(stdout); }
 static inline void unityOutputComplete(void) {}
 #endif
