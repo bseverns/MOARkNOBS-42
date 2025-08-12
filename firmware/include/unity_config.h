cad544251c0c3916e157cd22f272ed55761923ac
Ben Severns <severns3@gmail.com>
2025-08-12 11:21:36 -0500

    Stop double-defining Unity output hooks

--- firmware/include/unity_config.h
+++ firmware/include/unity_config.h
@@ -23,10 +23,18 @@ void unityOutputComplete();
 }
 #endif
 
+#ifndef UNITY_OUTPUT_START
 #define UNITY_OUTPUT_START(b)      unityOutputStart(b)
+#endif
+#ifndef UNITY_OUTPUT_CHAR
 #define UNITY_OUTPUT_CHAR(c)      unityOutputChar(c)
+#endif
+#ifndef UNITY_OUTPUT_FLUSH
 #define UNITY_OUTPUT_FLUSH()      unityOutputFlush()
+#endif
+#ifndef UNITY_OUTPUT_COMPLETE
 #define UNITY_OUTPUT_COMPLETE()   unityOutputComplete()
+#endif
 
 #if !defined(ARDUINO) && !defined(TEENSYDUINO) && !defined(UNIT_TEST)
 static inline void unityOutputStart(unsigned long baudrate) { (void)baudrate; }
