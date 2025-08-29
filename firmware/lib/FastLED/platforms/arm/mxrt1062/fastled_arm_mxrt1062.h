6d86fafe0b20e28f218800971f23a5cc77dfdfe4
Ben Severns <severns3@gmail.com>
2025-08-29 10:13:34 -0500

    fastled: drop k20 ghosts

--- firmware/lib/FastLED/platforms/arm/mxrt1062/fastled_arm_mxrt1062.h
+++ firmware/lib/FastLED/platforms/arm/mxrt1062/fastled_arm_mxrt1062.h
@@ -4,9 +4,17 @@
 #include "fastpin_arm_mxrt1062.h"
 #include "fastspi_arm_mxrt1062.h"
 #include "octows2811_controller.h"
-#include "../k20/ws2812serial_controller.h"
-#include "../k20/smartmatrix_t3.h"
 #include "clockless_arm_mxrt1062.h"
 #include "block_clockless_arm_mxrt1062.h"
+
+// Teensy 3-only helpers got the boot. If you need WS2812Serial or SmartMatrix,
+// bring back the k20 platform from upstream.
+#if __has_include("../k20/ws2812serial_controller.h")
+#include "../k20/ws2812serial_controller.h"
+#endif
+
+#if __has_include("../k20/smartmatrix_t3.h")
+#include "../k20/smartmatrix_t3.h"
+#endif
 
 #endif
