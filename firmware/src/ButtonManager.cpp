d4bd5717e5e326e476d83e54ea6507ad9dda10d7
Ben Severns <severns3@gmail.com>
2025-09-06 09:02:47 -0500

    style: clang-smack ButtonManager and polish README

--- firmware/src/ButtonManager.cpp
+++ firmware/src/ButtonManager.cpp
@@ -75,8 +75,8 @@ inline void setMuxFast(const uint8_t selPins[4], uint8_t index) {
 ButtonManager::ButtonManager(const HardwareConfig &config, const uint8_t *controlPins,
                              PotentiometerManager *potentiometerManager)
     : _cfg(config), _controlPins(controlPins), _potentiometerManager(potentiometerManager),
-      activeMode(0), activeARGMethod(0), argEnvelopeA(0), argEnvelopeB(1),
-      _pendingEfSlot(-1), _efAssignDeadline(0) {
+      activeMode(0), activeARGMethod(0), argEnvelopeA(0), argEnvelopeB(1), _pendingEfSlot(-1),
+      _efAssignDeadline(0) {
     for (int i = 0; i < NUM_VIRTUAL_BUTTONS + NUM_CONTROL_BUTTONS; i++) {
         buttonStates[i] = false;
         lastDebounceTimes[i] = 0;
