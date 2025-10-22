6cfdd69590f3ae23e460cb062331c312a1d05eb3
Ben Severns <severns3@gmail.com>
2025-10-21 21:32:27 -0500

    Fix bulk update layout test offsets

--- firmware/test/test_utility.cpp
+++ firmware/test/test_utility.cpp
@@ -9,8 +9,8 @@ void test_process_bulk_update_writes_expected_layout() {
     constexpr uint8_t numPotsUnderTest = 4;
     constexpr uint8_t sentinel = 0xAA;
 
-    const uint16_t layoutGuardEnd = EEPROM_POT_CC + numPotsUnderTest + 1;
-    for (uint16_t address = 0; address <= layoutGuardEnd; ++address) {
+    const uint16_t layoutGuardEnd = EEPROM_ENVELOPE_ASSIGNMENTS + numPotsUnderTest;
+    for (uint16_t address = 0; address < layoutGuardEnd; ++address) {
         EEPROM.update(address, sentinel);
     }
 
@@ -40,13 +40,14 @@ void test_process_bulk_update_writes_expected_layout() {
     Utility::processBulkUpdate(command, numPotsUnderTest);
 
     for (uint8_t i = 0; i < numPotsUnderTest; ++i) {
-        TEST_ASSERT_EQUAL_UINT8(assignments[i].channel,
-                                EEPROM.read(EEPROM_POT_CHANNELS + i));
+        TEST_ASSERT_EQUAL_UINT8(assignments[i].channel, EEPROM.read(EEPROM_POT_CHANNELS + i));
         TEST_ASSERT_EQUAL_UINT8(assignments[i].cc, EEPROM.read(EEPROM_POT_CC + i));
-        TEST_ASSERT_EQUAL_UINT8(sentinel, EEPROM.read(2 * i + 1));
     }
 
-    TEST_ASSERT_EQUAL_UINT8(sentinel, EEPROM.read(2 * numPotsUnderTest));
+    for (uint8_t i = 0; i < numPotsUnderTest; ++i) {
+        TEST_ASSERT_EQUAL_UINT8(sentinel, EEPROM.read(EEPROM_ENVELOPE_ASSIGNMENTS + i));
+    }
+
     TEST_ASSERT_EQUAL_UINT8(sentinel, EEPROM.read(EEPROM_POT_CHANNELS + numPotsUnderTest));
     TEST_ASSERT_EQUAL_UINT8(sentinel, EEPROM.read(EEPROM_POT_CC + numPotsUnderTest));
 }
