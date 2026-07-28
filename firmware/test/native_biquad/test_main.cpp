8c8e3f3cda4a86f84416da47b4a5dcba035b0926
Ben Severns <severns3@gmail.com>
2026-07-28 18:27:40 -0500

    native_biquad linkage

--- firmware/test/native_biquad/test_main.cpp
+++ firmware/test/native_biquad/test_main.cpp
@@ -4,6 +4,9 @@ void test_lowpass_preserves_dc_on_native_host();
 void test_highpass_rejects_dc_on_native_host();
 void test_native_filter_clamps_extreme_cutoffs();
 
+extern "C" void setUp() {}
+extern "C" void tearDown() {}
+
 int main() {
     UNITY_BEGIN();
     RUN_TEST(test_lowpass_preserves_dc_on_native_host);
